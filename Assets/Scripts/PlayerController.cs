9e57af571d36e3bebaf2dad00469081017761cf2
wbalaniucCentennialCollege <wbalaniu@my.centennialcollege.ca>
2018-02-07 14:18:24 -0500

    Player Movement Added

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // Used when performing physics calculations
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal"); // Returns a value between -1 and 1 whenever left, right, a, or d is pushed
+        float moveVertical = Input.GetAxis("Vertical"); // Reutn a value between -1 an d1 whenever up, down, w, or s is pushed
+
+        // Debug.Log("H= " + moveHorizontal + " V= " + moveVertical);
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        Rigidbody2D rBody = this.gameObject.GetComponent<Rigidbody2D>(); // Establishes a "connection" to the rigidbody2D component
+        rBody.velocity = movement * speed;
+    }
+}
