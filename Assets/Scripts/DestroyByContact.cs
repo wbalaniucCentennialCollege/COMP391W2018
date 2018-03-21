388a66713152285308016dc03894806df0b76a14
wbalaniucCentennialCollege <wbalaniu@my.centennialcollege.ca>
2018-03-21 14:12:56 -0400

    Added UI and Build

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -5,10 +5,23 @@ using UnityEngine;
 public class DestroyByContact : MonoBehaviour {
 
     public GameObject explosionAsteroid;
+    public GameObject explosionPlayer;
+    public int scoreValue = 10;
+
+    private GameController gameControllerScript;
 
 	// Use this for initialization
 	void Start () {
-		
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        
+        if(gameControllerObject != null)
+        {
+            gameControllerScript = gameControllerObject.GetComponent<GameController>();
+        }
+        if(gameControllerScript == null)
+        {
+            Debug.Log("Cannot find Game Controller script on Object");
+        }
 	}
 	
 	void OnTriggerEnter2D(Collider2D other)
@@ -19,7 +32,22 @@ public class DestroyByContact : MonoBehaviour {
             // Debug.Log("DestroyByContact");
         }
 
-        Instantiate(explosionAsteroid, this.transform.position, this.transform.rotation);
+        if (other.tag == "Player")
+        {
+            Vector3 deltaP = (this.transform.position + other.transform.position) / 2; // Vector between player and asteroid
+
+            // Create our explosion animation
+            Instantiate(explosionPlayer, deltaP, other.transform.rotation);
+            // Instantiate(explosionPlayer, other.transform.position, other.transform.rotation);
+
+            // GAME OVER!!!
+            gameControllerScript.GameOver();
+        }
+        else
+        {
+            Instantiate(explosionAsteroid, this.transform.position, this.transform.rotation);
+            gameControllerScript.AddScore(scoreValue);
+        }
 
         Destroy(other.gameObject); // Destroy the other thing (laser)
         Destroy(this.gameObject); // Destroying this thing (the asteroid)
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// NEW USING STATEMENTS
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject hazard; // What are we spawning?
+    public Vector2 spawnValue; // Where do we spawn our hazards?
+    public int hazardCount; // How many hazards per wave?
+    public float startWait; // How long until the first wave?
+    public float spawnWait; // How long between each hazard in each wave?
+    public float waveWait; // How long between each wave of enemies?
+
+    public Text scoreText;
+    public Text restartText;
+    public Text gameOverText;
+
+    private bool gameOver;
+    private bool restart;
+    private int score;
+
+	// Use this for initialization
+	void Start () {
+        score = 0;
+
+        scoreText.text = "";
+        restartText.text = "";
+        gameOverText.text = "";
+
+        UpdateScore();
+
+        StartCoroutine(SpawnWaves());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(restart)
+        {
+            if(Input.GetKeyDown(KeyCode.R))
+            {
+                // THE OLD WAY (DON'T USE THIS ONE)
+                // Application.LoadLevel(Application.loadedLevel);
+                // SceneManager.LoadScene("Spaceshooter"); // <-- This works fine but prone to error
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); // <-- Better but more complicated
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+	}
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait); // Pause. How long do we wait for the first wave?
+        while(true)
+        {
+            for(int i = 0; i < hazardCount; i++)
+            {
+                Vector2 spawnPosition = new Vector2(spawnValue.x, Random.Range(-spawnValue.y, spawnValue.y));
+                //                                      12                          -3.5          3.5
+                Quaternion spawnRotation = Quaternion.identity; // Default rotation.
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait); // Wait time between spawning each asteroid
+            }
+            yield return new WaitForSeconds(waveWait);
+
+            if(gameOver)
+            {
+                restartText.gameObject.SetActive(true);
+                restartText.text = "Press R for Restart";
+                restart = true;
+                break;
+            }
+        }
+    }
+
+    // Update the score text
+    void UpdateScore()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        // score = score + newScoreValue;
+        // Debug.Log("Score is " + score);
+        UpdateScore();
+    }
+
+    public void GameOver()
+    {
+        // Debug.Log("Game is Over");
+        gameOverText.gameObject.SetActive(true);
+        gameOverText.text = "Game Over";
+        gameOver = true;
+    }
+}
